e15716cd06fbc6bf0252390e3ebfdc6254c908df
Chirayu Marathe <47413231+Cam-98@users.noreply.github.com>
2021-12-03 15:27:48 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
 npm install -g ganache-cli
-npm install -g npm@8.2.0
 npm install -g truffle
